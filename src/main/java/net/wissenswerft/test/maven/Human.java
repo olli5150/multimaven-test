a8859b919933d933a8a6349b6532b345c49f4323
Oliver Weichert <oliver.weichert@wissenswerft.net>
2013-09-02 15:51:24 +0200

    add added files

---
+++ src/main/java/net/wissenswerft/test/maven/Human.java
@@ -0,0 +1,25 @@
+package net.wissenswerft.test.maven;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Admin
+ * Date: 02.09.13
+ * Time: 15:41
+ * To change this template use File | Settings | File Templates.
+ */
+@Log4j
+@AllArgsConstructor
+@Getter
+@Setter
+public class Human {
+
+    private String firstname;
+
+    private String lastName;
+
+}
